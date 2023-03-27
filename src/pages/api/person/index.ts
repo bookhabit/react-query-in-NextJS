b31c413ba6d3d4bb700aff45feed18d1ba1940e9
bookhabit <qnrgoqlt@naver.com>
2023-03-28 00:46:40 +0900

    useQuery사용과 devtool

--- src/pages/api/person/index.ts
+++ src/pages/api/person/index.ts
@@ -2,5 +2,6 @@ import { NextApiRequest,NextApiResponse } from 'next';
 import { IPerson } from '@/lib/interfaces/IPerson';
 
 export default (_req:NextApiRequest,res:NextApiResponse<IPerson>):void=>{
+    console.log('getting person')
     res.status(200).json({id:'1',name:'John',age:25})
 }
--- src/pages/person/index.tsx
+++ src/pages/person/index.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import {useQuery,UseQueryResult} from "@tanstack/react-query"
+import { IPerson } from '@/lib/interfaces/IPerson';
 
 const PersonPage:React.FC = () => {
+    const {isLoading,isError,error,data}:UseQueryResult<IPerson,Error> = useQuery<IPerson,Error,IPerson>(
+        ['person'],
+        async ()=>{
+            const res= await fetch('/api/person')
+            return res.json();
+        }
+    )
+    if(isLoading) return <p>Loading...</p>
+    if(isError) return <p>Error is : {error.message}</p>
     return (
         <div>
-            Person
+            <p>{data?.id}</p>
+            <p>{data?.name}</p>
+            <p>{data?.age}</p>
         </div>
     );
 };
