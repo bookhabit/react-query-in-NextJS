c3d98a53195d9153b71de2c65590e5c1f5c65084
bookhabit <qnrgoqlt@naver.com>
2023-03-28 01:33:39 +0900

    useQuery options

--- src/pages/person/index.tsx
+++ src/pages/person/index.tsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import {useQuery,UseQueryResult} from "@tanstack/react-query"
 import { IPerson } from '@/lib/interfaces/IPerson';
+import Link from 'next/link';
+
+const fetchPerson = async ():Promise<IPerson>=>{
+    const res= await fetch('/api/person')
+    if(res.ok){
+        return res.json();
+    }
+    throw new Error('Network response not ok')
+}
+
 
 const PersonPage:React.FC = () => {
     const {isLoading,isError,error,data}:UseQueryResult<IPerson,Error> = useQuery<IPerson,Error,IPerson>(
         ['person'],
-        async ()=>{
-            const res= await fetch('/api/person')
-            return res.json();
+        fetchPerson,
+        {
+            staleTime:5*1000
         }
     )
     if(isLoading) return <p>Loading...</p>
     if(isError) return <p>Error is : {error.message}</p>
     return (
         <div>
-            <p>{data?.id}</p>
+            <Link href={`/person/${data?.id}`}>
+                <p>{data?.id}</p>
+            </Link>
             <p>{data?.name}</p>
             <p>{data?.age}</p>
         </div>
